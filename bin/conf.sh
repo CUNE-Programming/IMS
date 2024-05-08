c711fc51c1758f36ebb800380d40f2b87777326d
Ian Kollipara <ian.kollipara@gmail.com>
2024-05-08 07:28:23 -0500

    fix: `bin/conf.sh` to use `vendor/bin/duster`
    Updated the `bin/conf.sh` script to use `vendor/bin/duster` instead of
    `duster` to ensure that the correct version of Duster is used.

--- bin/conf.sh
+++ bin/conf.sh
@@ -7,7 +7,7 @@
 REPO_HOME=$(git rev-parse --show-toplevel)
 
 # Add Shell Alias for Duster
-alias duster='$REPO_HOME/vendor/duster'
+alias duster='$REPO_HOME/vendor/bin/duster'
 alias format='duster fix'
 alias lint='duster lint'
 alias artisan='php $REPO_HOME/artisan'
